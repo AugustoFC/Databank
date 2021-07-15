98e8de68e66e904e55a24a65b2024e677fb73d69
AugustoFC <augusto.lfc.19@gmail.com>
2021-07-14 20:37:56 -0500

    Se mejora el prototipo

--- js/script_popup.js
+++ js/script_popup.js
@@ -2,12 +2,22 @@ var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
   btnAbrirPopup2 = document.getElementById('btn-abrir-popup2'),
 	overlay = document.getElementById('overlay'),
 	popup = document.getElementById('popup'),
+	overlay2 = document.getElementById('overlay2'),
+	popup2 = document.getElementById('popup2'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+	btnCerrarPopup2 = document.getElementById('btn-cerrar-popup2');
+
 
 btnAbrirPopup.addEventListener('click', function(){
 	overlay.classList.add('active');
 	popup.classList.add('active');
 });
+
+btnAbrirPopup2.addEventListener('click', function(){
+	overlay2.classList.add('active');
+	popup2.classList.add('active');
+});
+
 
 
 
@@ -15,4 +25,10 @@ btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
 	overlay.classList.remove('active');
 	popup.classList.remove('active');
+});
+
+btnCerrarPopup2.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay2.classList.remove('active');
+	popup2.classList.remove('active');
 });
